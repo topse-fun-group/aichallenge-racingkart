a1c7ad39cf0c6c301d02ada6f8e91bae690368dc
Project Name <project@example.com>
2026-08-15 09:59:24 +0900

    ヘッドレスモード設定解除

--- aichallenge/build_autoware.bash
+++ aichallenge/build_autoware.bash
@@ -28,7 +28,7 @@ source /autoware/install/setup.bash
 cd ./workspace
 
 # NOTE: gyro_odometer exists in the Autoware underlay, so allow overriding in this overlay workspace.
-colcon build --symlink-install --allow-overriding gyro_odometer --cmake-args -DCMAKE_BUILD_TYPE=Release --parallel-workers 1
+colcon build --symlink-install --allow-overriding gyro_odometer --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 echo "[build_autoware] Build successful."
 
--- aichallenge/simulator_scripts/dev.sh
+++ aichallenge/simulator_scripts/dev.sh
@@ -22,8 +22,7 @@ exec $AWSIM_DIRECTORY/AWSIM.x86_64 \
     --wall-recovery on \
     --ranking off \
     --camera off \
-    --lidar off \
-    -headless
+    --lidar off
 
 # Cameraを使う場合 : --camera cpu or gpu
 # LiDARを使う場合 : --lidar cpu or gpu
